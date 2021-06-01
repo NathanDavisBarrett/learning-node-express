33a2ec4d159ac532fceba47a6b7ae51e882ac742
NathanDavisBarrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2021-06-01 10:48:34 -0600

    no console 2

--- lesson2/front-end/src/views/Create.vue
+++ lesson2/front-end/src/views/Create.vue
@@ -40,10 +40,9 @@ export default {
         this.problem = "";
         this.creating = false;
         return true;
+      } catch (error) {
+        //console.log(error);
       }
-      // catch (error) {
-      //   console.log(error);
-      // }
 
     },
   }
